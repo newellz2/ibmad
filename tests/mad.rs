04ffad5a44bd9b4715d723838c88faccc02a50a5
Zachary Newell <52436542+newellz2@users.noreply.github.com>
2025-05-26 08:25:46 -0700

    Return agent id and add tests

--- tests/mad.rs
+++ tests/mad.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod mad_tests {
+    use std::path::Path;
     use ibmad::mad::{open_port, register_agent};
 
 
@@ -7,6 +8,11 @@ mod mad_tests {
     fn open_port_success() {
 
         let _ = env_logger::try_init();
+
+        if !Path::new(ibmad::SYS_INFINIBAND).exists() {
+            eprintln!("IB system path not found, skipping test");
+            return;
+        }
 
         match  ibmad::cas::get_cas(){
             Ok(cas) =>{
@@ -32,6 +38,11 @@ mod mad_tests {
 
         let _ = env_logger::try_init();
 
+        if !Path::new("/dev/infiniband/umad0").exists() {
+            eprintln!("UMAD device not found, skipping test");
+            return;
+        }
+
         match  ibmad::cas::get_cas(){
             Ok(cas) =>{
                 assert!(cas.len() > 0, "No CAs found.");
@@ -39,7 +50,8 @@ mod mad_tests {
                 match open_port(hca) {
                     Ok(mut port) => {
                         log::debug!("register_agent_success - Opened IB MAD Port: {:?}", port);
-                        register_agent(&mut port, ibmad::IB_PERFORMANCE_MGMT_CLASS);
+                        let r = register_agent(&mut port, ibmad::IB_PERFORMANCE_MGMT_CLASS);
+                        assert!(r.is_ok(), "Failed to register agent: {:?}", r);
                     },
                     Err(e) => {
                         assert!(false, "{}", format!("Error opening port: {:?}", e));
@@ -56,6 +68,10 @@ mod mad_tests {
     fn send_success() {
 
         let _ = env_logger::try_init();
+        if !Path::new("/dev/infiniband/umad0").exists() {
+            eprintln!("UMAD device not found, skipping test");
+            return;
+        }
 
         match  ibmad::cas::get_cas(){
             Ok(cas) =>{
@@ -64,8 +80,9 @@ mod mad_tests {
                 match open_port(hca) {
                     Ok(mut port) => {
                         log::debug!("send_success - Opened IB MAD Port: {:?}", port);
-                        register_agent(&mut port, 0x81);
-                        ibmad::mad::send(&mut port);
+                        if let Ok(_) = register_agent(&mut port, 0x81) {
+                            ibmad::mad::send(&mut port);
+                        }
                     },
                     Err(e) => {
                         assert!(false, "{}", format!("Error opening port: {:?}", e));
