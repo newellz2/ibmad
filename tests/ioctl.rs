a2d9a5abc3c21b385eb308e7be846cf25c3d475b
Zach N <znewell@nvidia.com>
2026-03-28 22:40:32 +0300

    Changed CA and iotctl filenames.

---
+++ tests/ioctl.rs
@@ -0,0 +1,155 @@
+#[cfg(test)]
+mod ioctl_tests {
+    use std::mem::MaybeUninit;
+    use std::path::Path;
+
+    #[test]
+    fn ib_enable_pkey_success() {
+        if !Path::new("/dev/infiniband/umad0").exists() {
+            eprintln!("UMAD device not found, skipping test");
+            return;
+        }
+
+        match std::fs::File::options()
+            .read(true)
+            .write(true)
+            .open("/dev/infiniband/umad0")
+        {
+            Ok(file) => {
+                let fd = std::os::fd::AsRawFd::as_raw_fd(&file);
+
+                // Enable PKeys
+                let r = unsafe { ibmad::ib_user_mad_enable_pkey(fd) };
+
+                match r {
+                    Ok(i) => {
+                        assert!(i > -1, "PKey enabled")
+                    }
+                    Err(_) => {
+                        assert!(false, "Failed to enable Pkeys")
+                    }
+                }
+            }
+            Err(_) => {
+                //Failed
+            }
+        }
+    }
+
+    #[test]
+    fn ib_user_mad_reg_req_success() {
+        let mut req = ibmad::ib_user_mad_reg_req {
+            id: 0,
+            method_mask: unsafe { MaybeUninit::<[u32; 4]>::zeroed().assume_init() },
+            qpn: 0,
+            mgmt_class: 1,
+            mgmt_class_version: 1,
+            oui: unsafe { MaybeUninit::<[u8; 3]>::zeroed().assume_init() },
+            rmpp_version: 0,
+        };
+
+        if !Path::new("/dev/infiniband/umad0").exists() {
+            eprintln!("UMAD device not found, skipping test");
+            return;
+        }
+
+        match std::fs::File::options()
+            .read(true)
+            .write(true)
+            .open("/dev/infiniband/umad0")
+        {
+            Ok(file) => {
+                let fd = std::os::fd::AsRawFd::as_raw_fd(&file);
+
+                let req_ptr: *mut ibmad::ib_user_mad_reg_req = &mut req;
+
+                // Enable PKeys
+                let r = unsafe { ibmad::ib_user_mad_enable_pkey(fd) };
+
+                match r {
+                    Ok(i) => {
+                        assert!(i > -1, "PKey enabled")
+                    }
+                    Err(_) => {
+                        assert!(false, "Failed to enable Pkeys")
+                    }
+                }
+
+                // Register agent
+                let r = unsafe { ibmad::ib_user_mad_register_agent(fd, req_ptr) };
+
+                match r {
+                    Ok(i) => {
+                        assert!(i > -1, "Agent registered")
+                    }
+                    Err(_) => {
+                        assert!(false, "Failed to register agent")
+                    }
+                }
+            }
+            Err(_) => {
+                //Failed
+            }
+        }
+    }
+
+    #[test]
+    fn ib_user_mad_reg_req2_success() {
+        let mut req = ibmad::ib_user_mad_reg_req2 {
+            id: 0,
+            qpn: 0,
+            mgmt_class: 1,
+            mgmt_class_version: 1,
+            res: 0,
+            flags: 0,
+            method_mask: unsafe { MaybeUninit::<[u64; 2]>::zeroed().assume_init() },
+            oui: 0,
+            rmpp_version: 0,
+            reserved: unsafe { MaybeUninit::<[u8; 3]>::zeroed().assume_init() },
+        };
+
+        if !Path::new("/dev/infiniband/umad0").exists() {
+            eprintln!("UMAD device not found, skipping test");
+            return;
+        }
+
+        match std::fs::File::options()
+            .read(true)
+            .write(true)
+            .open("/dev/infiniband/umad0")
+        {
+            Ok(file) => {
+                let fd = std::os::fd::AsRawFd::as_raw_fd(&file);
+
+                let req_ptr: *mut ibmad::ib_user_mad_reg_req2 = &mut req;
+
+                // Enable PKeys
+                let r = unsafe { ibmad::ib_user_mad_enable_pkey(fd) };
+
+                match r {
+                    Ok(i) => {
+                        assert!(i > -1, "PKey enabled")
+                    }
+                    Err(_) => {
+                        assert!(false, "Failed to enable PKeys")
+                    }
+                }
+
+                // Register agent
+                let r = unsafe { ibmad::ib_user_mad_register_agent2(fd, req_ptr) };
+
+                match r {
+                    Ok(i) => {
+                        assert!(i > -1, "Agent2 registered")
+                    }
+                    Err(_) => {
+                        assert!(false, "Failed to register agent2")
+                    }
+                }
+            }
+            Err(_) => {
+                //Failed
+            }
+        }
+    }
+}
